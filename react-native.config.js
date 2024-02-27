57a83a20b1d9b638adba2e1fe9e94cd7539a4758
appricotblue <git2@aindriya.co.in>
2024-02-27 10:18:50 +0530

    first commit

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   preset: 'react-native',
+// };
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets/fonts/Jost/static'],
+  // assets: ['./src/assets/fonts/Aaargh'],
+};
